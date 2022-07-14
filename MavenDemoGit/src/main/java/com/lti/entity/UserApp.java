42e9862f94b4199c5f10a0cb1b41cca3d266143d
Abhishek6387724595 <avasthiabhishek0033@gmail.com>
2022-07-14 12:49:29 +0530

    Fifth Commit(Chal jaa)

--- MavenDemoGit/src/main/java/com/lti/entity/UserApp.java
+++ MavenDemoGit/src/main/java/com/lti/entity/UserApp.java
@@ -4,6 +4,7 @@ public class UserApp {
 	public static void main(String[] arg) {
 		System.out.println("Hello World Man");
 		System.out.println("Hello World Man");
+		System.out.println("Don Don");
 	}
 
 }
