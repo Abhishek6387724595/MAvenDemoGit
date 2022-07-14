70a02fcfa373fb6db7f7f0be30b2a98c850ae547
Abhishek Awasthi <avasthiabhishek0033@gmail.com>
2022-07-14 12:51:56 +0530

    Create ClientApp.java

---
+++ MavenDemoGit/src/main/java/com/lti/entity/ClientApp.java
@@ -0,0 +1,10 @@
+package com.lti.entity;
+
+public class ClientApp {
+	public static void main(String[] arg) {
+		System.out.println("Hello World Man");
+		System.out.println("Hello World Man");
+		System.out.println("Don Don");
+	}
+
+}
